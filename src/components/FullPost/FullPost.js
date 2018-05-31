f43630276e4075e70140e1cf51a6b0ef285fd810
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-31 00:10:03 +0000

    added get and post requests

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './FullPost.css';
+import Axios from "axios";
+
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    loadedPost: null
+  };
 
-        );
-        return post;
+  componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.props.id !== this.state.loadedPost.id)
+      ) {
+        Axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${this.props.id}`
+        ).then(res => this.setState({ loadedPost: res.data }));
+      }
     }
+  }
+
+  render() {
+    let post = <p className="FullPost">Please select a Post!</p>;
+
+    if (this.props.id) {
+      post = <p className="FullPost">Loading ......</p>;
+    }
+
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    }
+
+    return post;
+  }
 }
 
 export default FullPost;

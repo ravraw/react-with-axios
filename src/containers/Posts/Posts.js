e48e9a4c7fc153874db1fb7fe362efa017f3a2d8
ravindra rawat <ravinrawat.wd@gmail.com>
2018-06-03 14:47:45 -0600

    added nested route to posts for fullpost

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 import Axios from "../../../src/axios";
 
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Post from "../../components/Post/Post";
+import FullPost from "../FullPost/FullPost";
 
 import "./Posts.css";
 
@@ -32,7 +34,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler(id) {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: "/" + id });
+    //this.props.history.push({"/" + id });
   }
 
   render() {
@@ -40,17 +43,27 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Link to={"/" + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          // <Link to={"/" + post.id} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          // </Link>
         );
       });
     }
-    return <section className="Posts">{posts}</section>;
+    return (
+      <div>
+        <section className="Posts">{posts}</section>;
+        <Route
+          path={this.props.match.url + "/:id"}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
   }
 }
 export default Posts;
